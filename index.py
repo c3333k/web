81d802bb1fc83d11d79ff7e83570af794b47646b
c3333k <61370746+c3333k@users.noreply.github.com>
2020-02-24 13:36:30 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,4 +1,29 @@
 #!usr/local/bin/python3
 print("Content-Type: text/html")
 print()
-print('Hello world')
+print('''<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>블록체인(Blockchain) - Welcome</title>
+</head>
+<body>
+  <h1><a href="index.html">블록체인(Blockchain)</a></h1>
+  <ol>
+    <li><a href="1.html">개념과 원리</a></li>
+    <li><a href="2.html">활용사례</a></li>
+    <li><a href="3.html">퍼블릭 블록체인과 프라이빗 블록체인</a></li>
+    <li><a href="4.html">관련용어</a></li>
+    <li><a href="5.html">관련인물</a></li>
+    <li><a href="6.html">관련소식</a></li>
+  </ol>
+  <p style="margin-top:70px;">
+  <h3><a href="https://ko.wikipedia.org/wiki/블록체인" target="_blank" title="위키백과 - 블록체인">블록체인이란?</a></h3>
+  <p>블록에 데이터를 담아 체인 형태로 연결, 수많은 컴퓨터에 동시에 이를 복제해 저장하는 <strong>분산형 데이터 저장 기술</strong>이다. <strong>공공 거래 장부</strong>라고도 부른다. 중앙 집중형 서버에 거래 기록을 보관하지 않고 거래에 모든 사용자에게 거래 내역을 보내 주며, <u>거래 때마다 모든 거래 참여자들이 정보를 공유하고 이를 대조해 데이터 위조나 변조를 할 수 없도록 돼 있다.</u></p>
+<p>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/rOnkutYO6aA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</p>
+</body>
+</html>
+''')
+
